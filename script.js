7caaad8c44c12ac7bfcc8d658cc8166881044974
syazakahah <156535764+syazakahah@users.noreply.github.com>
2024-05-22 15:25:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let numButtonClicks = 0;
+function buttonClicked() {
+    numButtonClicks = numButtonClicks + 1;
+    document.getElementById("mainDiv").textContent =
+        "Button Clicked times: " + numButtonClicks;
+}
+
+
+  function search() {
+      var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+      ];
+      }
+   
