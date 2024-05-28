da8578caa78812aed6e41b61cf4eead8362589e2
syazakahah <156535764+syazakahah@users.noreply.github.com>
2024-05-28 17:39:20 +0800

    Add files via upload

---
+++ Html Codes/script.js
@@ -0,0 +1,27 @@
+    
+
+    document.addEventListener('DOMContentLoaded', function() {
+            var urlParams = new URLSearchParams(window.location.search);
+            var query = urlParams.get('query');
+            
+            if (query) {
+                query = query.toLowerCase();
+                var sections = document.querySelectorAll('.scrollspy-example-2 div[id]');
+                
+                sections.forEach(function(section) {
+                    if (section.innerText.toLowerCase().includes(query)) {
+                        section.scrollIntoView({ behavior: 'smooth' });
+                        highlightSection(section);
+                    }
+                });
+            }
+        });
+
+        function highlightSection(section) {
+            section.style.backgroundColor = '#d3a0a0'; // Temporary highlight
+            setTimeout(function() {
+                section.style.backgroundColor = '';
+            }, 2000); // Remove highlight after 2 seconds
+        }
+
+   
